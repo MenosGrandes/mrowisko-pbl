87bee594dbf0dba339961517cb2afc6d1ed83e5a
x1gargamel1x@gmail.com <x1gargamel1x@gmail.com@08e183d9-be55-374b-ec7d-02f978a6822b>
2014-04-17 10:31:45 +0000

    Dodana klasa Command. Rozbudowane klasy logiki o nowe pola i metody

--- trunk/Mrowisko/KlasyZJednostkami/KlasyZJednostakmi/Unit.cs
+++ trunk/Mrowisko/KlasyZJednostkami/KlasyZJednostakmi/Unit.cs
@@ -34,6 +34,43 @@ namespace Logic
             get { return strength; }
             set { strength = value; }
         }
+
+        protected float range;
+
+        public float Range
+        {
+            get { return range; }
+            set { range = value; }
+        }
+
+        protected float speed;
+
+        public float Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+
+        protected float buildingTime;
+
+        public float BuildingTime
+        {
+            get { return buildingTime; }
+            set { buildingTime = value; }
+        }
+
+        protected int cost = 0;
+
+        public int Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        } 
+
+
+
+
         protected List<Skill> skillsList;
 
         public List<Skill> SkillsList
@@ -41,15 +78,44 @@ namespace Logic
             get { return skillsList; }
             set { skillsList = value; }
         }
+
+        
        
 
-         Unit(int hp, float armor, float strength)
+         Unit(ContentManager content, int hp, float armor, float strength, float range, int cost, float buildingTime, LoadModel model):base(content, model)
         {
             
             this.hp = hp;
             this.armor = armor;
             this.strength = strength;
+            this.range = range;
+            this.cost = cost;
+            this.buildingTime = buildingTime;
+
+            
+
         }
+
+        public Vector3 getPosition()
+         {
+             return this.model.Position;
+         }
+        public void setPosition(Vector3 position)
+        {
+            this.model.Position = position;
+        }
+
+        public Vector3 getRotation()
+        {
+            return this.model.Rotation;
+        }
+
+        public void setRottation(Vector3 rotation)
+        {
+            this.model.Rotation = rotation;
+        }
+
+
 
     }
 }
