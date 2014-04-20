b4d9b926667d1bf234f22966f4ac684926f95fa2
szafir701 <szafir701@08e183d9-be55-374b-ec7d-02f978a6822b>
2014-04-20 21:46:39 +0000

    dodalem poruszanie za pomoca myszki bez plynnosci ruchu

--- Mrowisko/Mrowisko/Mrowisko/Game1.cs
+++ Mrowisko/Mrowisko/Mrowisko/Game1.cs
@@ -202,17 +202,87 @@ new Vector3(100), GraphicsDevice, Content);
         }
 
         void updateAnt(GameTime gameTime)
-        {            KeyboardState keyState = Keyboard.GetState();
+        {
+            KeyboardState keyState = Keyboard.GetState();
+            MouseState MS = Mouse.GetState();
+            Vector2 mouse_pos = new Vector2(MS.X, MS.Y);
+            Vector3 mouse3d2 = CalculateMouse3DPosition();
+            float Speed = (float)0.02;
+            if (MS.LeftButton == ButtonState.Pressed)
+            {
+
+                if (mouse3d2.X > models[0].Position.X)
+                {
+                    while (mouse3d2.X > models[0].Position.X)
+                    {
+                        models[0].Position += Vector3.Right * Speed;
+                    }
+
+                }
+
+                if (mouse3d2.X < models[0].Position.X)
+                {
+                    while (mouse3d2.X < models[0].Position.X)
+                    {
+                        models[0].Position += Vector3.Left * Speed;
+                    }
+
+                }
 
 
-      
+                if (mouse3d2.Z > models[0].Position.Z)
+                {
 
-            if (keyState.IsKeyDown(Keys.Up) ) models[0].Position+= Vector3.Forward*100;
-            if (keyState.IsKeyDown(Keys.Down)) models[0].Position += Vector3.Backward * 100;
-            if (keyState.IsKeyDown(Keys.Left)) models[0].Position += Vector3.Left * 100;
-            if (keyState.IsKeyDown(Keys.Right)) models[0].Position += Vector3.Right * 100;
-                                   
+                    while (mouse3d2.Z > models[0].Position.Z)
+                    {
+                        models[0].Position += Vector3.Backward * Speed;
+                    }
+                }
+
+                if (mouse3d2.Z < models[0].Position.Z)
+                {
+                    while (mouse3d2.Z < models[0].Position.Z)
+                    {
+                        models[0].Position += Vector3.Forward * Speed;
+                    }
+                }
+
+
+                if (keyState.IsKeyDown(Keys.Up)) models[0].Position += Vector3.Forward * 100;
+                if (keyState.IsKeyDown(Keys.Down)) models[0].Position += Vector3.Backward * 100;
+                if (keyState.IsKeyDown(Keys.Left)) models[0].Position += Vector3.Left * 100;
+                if (keyState.IsKeyDown(Keys.Right)) models[0].Position += Vector3.Right * 100;
+
+            }
         }
+
+
+        private Vector3 CalculateMouse3DPosition()
+        {
+            Plane GroundPlane = new Plane(0, 1, 0, 0); // x - lewo prawo Z- gora dol
+            int mouseX = Mouse.GetState().X;
+            int mouseY = Mouse.GetState().Y;
+
+            Vector3 nearsource = new Vector3((float)mouseX, (float)mouseY, 0f);
+            Vector3 farsource = new Vector3((float)mouseX, (float)mouseY, 1f);
+
+            Matrix world = Matrix.CreateTranslation(0, 0, 0);
+
+            Vector3 nearPoint = device.Viewport.Unproject(nearsource,
+                camera.Projection, camera.View, Matrix.Identity);
+
+            Vector3 farPoint = device.Viewport.Unproject(farsource,
+                camera.Projection, camera.View, Matrix.Identity);
+
+            Vector3 direction = farPoint - nearPoint;
+            direction.Normalize();
+            Ray pickRay = new Ray(nearPoint, direction);
+            float? position = pickRay.Intersects(GroundPlane);
+            return pickRay.Position + pickRay.Direction * position.Value;
+            
+        }
+    
+    
     }
 }
 
