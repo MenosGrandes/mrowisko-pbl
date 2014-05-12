d3b1364bd30e31dad306553c91c7860f783abb8b
FilipMenos@gmail.com <FilipMenos@gmail.com@08e183d9-be55-374b-ec7d-02f978a6822b>
2014-05-12 10:06:47 +0000

    Dodane kolejne 2 farmy. Wystarczy tylko pozmieniać wartości czasowe żeby robiły w innym czasie surowce. Dodana nastepna mrówka i zmienione troche kolizje. Bo może być tak że jest kolizja mrówka-kamień ale i kamień-mrówka. Wiec trzeba sprawdzać.

--- trunk/Mrowisko/KlasyZJednostkami/KlasyZJednostakmi/Building/AntBuildings/SeedFarms/DicentraFarm.cs
+++ trunk/Mrowisko/KlasyZJednostkami/KlasyZJednostakmi/Building/AntBuildings/SeedFarms/DicentraFarm.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Map;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,15 +7,26 @@ using System.Text;
 
 namespace Logic.Building.AntBuildings.SeedFarms
 {
-    public class DicentraFarm : SeedFarm
-    {
 
-        public DicentraFarm()
-            : base()
-        { }
-        public override void Draw(Matrix view, Matrix Projection)
-        {
-            //Console.WriteLine(this.GetType());
+    public class DicentraFarm : SeedFarm    {
+        public DicentraFarm( LoadModel model, int _capacity, int _durability, int _cost, float _buildingTime,float cropTime):base( model,_capacity,_durability,_cost,_buildingTime,cropTime)
+        { 
+        
         }
+         public override void  Draw(Matrix View, Matrix Projection)
+        {
+            model.Draw(View, Projection);
+        }
+        public DicentraFarm()
+        { }
+        public override Logic.Meterials.Material addCrop()
+        {  
+            return new Logic.Meterials.Dicentra();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            timeElapsed += (float)gameTime.ElapsedGameTime.TotalMilliseconds/100;
+        }
+
     }
 }
