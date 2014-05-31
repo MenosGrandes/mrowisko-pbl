60841758697e0c0d6d7193e6ade3cb85b450cb04
x1gargamel1x@gmail.com <x1gargamel1x@gmail.com@08e183d9-be55-374b-ec7d-02f978a6822b>
2014-05-31 17:29:37 +0000

    Dodane efekty czasteczkowe

---
+++ trunk/Mrowisko/ParticleSystems/ParticleSystems/ProjectileTrailParticleSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Particles.ParticleSystems
+{
+    public class ProjectileTrailParticleSystem : ParticleSystem
+    {
+        public ProjectileTrailParticleSystem(Game game, ContentManager content)
+            : base(game, content)
+        { }
+
+
+        protected override void InitializeSettings(ParticleSettings settings)
+        {
+            settings.TextureName = "Textures/Particles/smoke";
+
+            settings.MaxParticles = 1000;
+
+            settings.Duration = TimeSpan.FromSeconds(3);
+
+            settings.DurationRandomness = 1.5f;
+
+            settings.EmitterVelocitySensitivity = 0.1f;
+
+            settings.MinHorizontalVelocity = 0;
+            settings.MaxHorizontalVelocity = 1;
+
+            settings.MinVerticalVelocity = -1;
+            settings.MaxVerticalVelocity = 1;
+
+            settings.MinColor = new Color(64, 96, 128, 255);
+            settings.MaxColor = new Color(255, 255, 255, 128);
+
+            settings.MinRotateSpeed = -4;
+            settings.MaxRotateSpeed = 4;
+
+            settings.MinStartSize = 1;
+            settings.MaxStartSize = 3;
+
+            settings.MinEndSize = 4;
+            settings.MaxEndSize = 11;
+        }
+    }
+}
