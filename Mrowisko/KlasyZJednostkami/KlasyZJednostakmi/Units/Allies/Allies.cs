ffc0218290a4caf374fb384c0f40537b479a7c97
szafir701 <szafir701@08e183d9-be55-374b-ec7d-02f978a6822b>
2014-06-11 19:01:19 +0000

    dodalem dwie klasy. Klasa Beetle odpowada za zuka Klasa SunDew za rosiczke

--- Mrowisko/KlasyZJednostkami/KlasyZJednostakmi/Units/Allies/Allies.cs
+++ Mrowisko/KlasyZJednostkami/KlasyZJednostakmi/Units/Allies/Allies.cs
@@ -7,5 +7,16 @@ namespace Logic
 {
     class Allies:Unit
     {
+     public Allies(int hp, float armor, float strength, float range, int cost, float buildingTime, LoadModel model, float atackInterval)
+              : base(hp, armor, strength, range, cost, buildingTime, model, atackInterval)
+        {
+            base.elapsedTime = 0;
+           }
+        public Allies(LoadModel model):base(model)
+           { base.elapsedTime = 0; }
+        public Allies()
+            : base()
+        { base.elapsedTime = 0; }
+   
     }
 }
---
+++ Mrowisko/KlasyZJednostkami/KlasyZJednostakmi/Units/Predators/SunDew.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+namespace Logic.Units.Predators
+{
+    class SunDew:Unit
+    {
+    
+     public List<InteractiveModel> Ants = new List<InteractiveModel>();
+        private float Scope;
+        private float AttackSpeed;
+        private int Damage;
+
+        public SunDew(int hp, float armor, float strength, float range, int cost, float buildingTime, LoadModel model, int maxCapacity, float gaterTime, float atackInterval,float Scope,float AttackSpeed, int Damage)
+            : base(hp, armor, strength, range, cost, buildingTime, model, atackInterval)
+    {
+        this.Scope = Scope;
+        this.AttackSpeed=AttackSpeed;
+        this.Damage=Damage;
+    }
+        public void update(GameTime gameTime)
+        {
+                foreach(Unit model in Ants)
+                {
+                    float lenght = (float)Math.Sqrt(Math.Pow(model.Model.Position.X - this.Model.Position.X, 2.0f) + Math.Pow(model.Model.Position.Z - this.Model.Position.Z, 2.0f));
+                    if(lenght<=Scope)
+                    {
+                        AttackSpeed += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    if (AttackSpeed > 2.0f)
+                    {
+                        if (model.Hp > 0)
+                            model.Hp -= Damage;
+                        AttackSpeed = 0.0f;
+                    }
+                    }
+                }
+            
+
+        }
+    
+    }
+     
+    }
+
