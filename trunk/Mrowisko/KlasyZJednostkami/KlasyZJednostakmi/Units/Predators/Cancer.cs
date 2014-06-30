160bc44ad24fa36f426103cabf2085029a0b02c0
x1gargamel1x@gmail.com <x1gargamel1x@gmail.com@08e183d9-be55-374b-ec7d-02f978a6822b>
2014-06-30 22:00:24 +0000

    rak poprawiony

--- trunk/Mrowisko/KlasyZJednostkami/KlasyZJednostakmi/Units/Predators/Cancer.cs
+++ trunk/Mrowisko/KlasyZJednostkami/KlasyZJednostakmi/Units/Predators/Cancer.cs
@@ -16,6 +16,7 @@ namespace Logic.Units.Predators
         private bool has_reached = false;
         private int rgn = 350;
         private int damage = 30;
+        private int counter = 0;
         public Cancer(int hp, float armor, float strength, float range, int cost, float buildingTime, LoadModel model, int maxCapacity, float gaterTime, float atackInterval, float Scope, float AttackSpeed, int Damage)
             : base(hp, armor, strength, range, cost, buildingTime, model, atackInterval)
         {
@@ -48,16 +49,22 @@ namespace Logic.Units.Predators
                       float spr=Vector2.Distance(new Vector2(this.Model.BoundingSphere.Center.X, this.Model.BoundingSphere.Center.Z),new Vector2(Ants[i].Model.BoundingSphere.Center.X, Ants[i].Model.BoundingSphere.Center.Z));
                     if (spr <= rgn && this != Ants[i])
                     {
+                        counter++;
                         if (Ants[i] is Unit && !(Ants[i] is Predator))
                         {
                             if (Vector2.Distance(new Vector2(this.Model.BoundingSphere.Center.X, this.Model.BoundingSphere.Center.Z), new Vector2(Ants[i].Model.BoundingSphere.Center.X, Ants[i].Model.BoundingSphere.Center.Z)) > this.Model.BoundingSphere.Radius * 1.75)
-                                this.reachTargetAutonomus(gameTime, Ants[i].Model.Position);
-                            else
                             {
-                                time_dmg += (float)gameTime.ElapsedGameTime.TotalMilliseconds/1000;
+                                this.model.switchAnimation("Walk");
+                                this.reachTargetAutonomus(gameTime, Ants[i].Model.Position);
+                                this.model.Rotation = new Vector3(this.model.Rotation.X, StaticHelpers.StaticHelper.TurnToFace(new Vector2(this.model.Position.X, this.model.Position.Z), new Vector2(Ants[i].Model.Position.X, Ants[i].Model.Position.Z), this.model.Rotation.Y, 1.05f), model.Rotation.Z);
+                            }
+                            else if (Vector2.Distance(new Vector2(this.Model.BoundingSphere.Center.X, this.Model.BoundingSphere.Center.Z), new Vector2(Ants[i].Model.BoundingSphere.Center.X, Ants[i].Model.BoundingSphere.Center.Z)) <= this.Model.BoundingSphere.Radius * 1.75)
+                            {
+                                this.model.switchAnimation("Atack");
+                                time_dmg += (float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000;
                                 if (time_dmg > 3.0f)
                                 {
-                                    this.model.switchAnimation("Atack");
+                                    
                                     Ants[i].hasBeenHit = true;
                                     if (Ants[i].foe == null)
                                     {
@@ -67,15 +74,25 @@ namespace Logic.Units.Predators
                                     ((Unit)Ants[i]).LifeBar.LifeLength -= ((Unit)Ants[i]).LifeBar.LifeLength * ((float)damage / Ants[i].MaxHp);
                                     time_dmg = 0;
                                 }
+                               
                             }
                             break;
                         }
                     }
 
                 }
+                if (counter == 0 && has_reached == true)
+                {
+                    this.model.switchAnimation("Idle");
+                }
+                else
+                {
+                    counter = 0;
+                }
             }
             else
             {
+                this.model.switchAnimation("Walk");
                 time += (float)gameTime.ElapsedGameTime.TotalSeconds;
                 if (time > time_to_move)
                 {
